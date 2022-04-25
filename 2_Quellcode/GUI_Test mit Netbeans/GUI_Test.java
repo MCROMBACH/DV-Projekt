1c8daeb61ac1a333a5660488a6116c15b05c60f0
MCROMBACH <mirco.rombach@hs-furtwangen.de>
2022-04-25 18:26:14 +0200

    Erster Versuch GUI Netbeans

---
+++ 2_Quellcode/GUI_Test mit Netbeans/GUI_Test.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gui_test;
+
+/**
+ *
+ * @author mirco
+ */
+public class GUI_Test {
+
+    public static void main(String[] args) {
+        LabyrinthFrom Laby1 = new LabyrinthFrom();
+        Laby1.setVisible(true);
+        Laby1.setResizable(false);
+       Laby1.setSize(400, 300);
+    }
+}
