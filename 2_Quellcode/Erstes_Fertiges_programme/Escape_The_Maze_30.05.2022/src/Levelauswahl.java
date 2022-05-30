296a27ce44b01812ec9a4424ca5d6f5d12c34fd1
MCROMBACH <mirco.rombach@hs-furtwangen.de>
2022-05-30 19:31:13 +0200

    neues Programm 30.05.2022

---
+++ 2_Quellcode/Erstes_Fertiges_programme/Escape_The_Maze_30.05.2022/src/Levelauswahl.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+
+
+
+public class Levelauswahl extends JPanel{
+	
+	JButton Level1 = new JButton();
+	JButton Level2 = new JButton();
+	JButton Level3 = new JButton();
+	static int[][]layout = new int [20][20];
+	
+	
+	Levelauswahl(){
+		
+		Level1 = new JButton();
+		Level1.setPreferredSize(new Dimension(100,30));
+		Level1.setText("Level1");
+		add(Level1);
+		Level1.addActionListener(new ALLevel1());
+		Level2 = new JButton();
+		Level2.setPreferredSize(new Dimension(100,30));
+		Level2.setText("Level2");
+		add(Level2);
+		Level2.addActionListener(new ALLevel2());
+		Level3 = new JButton();
+		Level3.setPreferredSize(new Dimension(100,30));
+		Level3.setText("Level3");
+		add(Level3);
+		Level3.addActionListener(new ALLevel3());
+	}
+	
+	class ALLevel1 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			//Rahmen.inFrame("Level1",new Level1() , 1920, 1080);
+			StartMenue.levelFrame= Rahmen.inFrame("Level1", new Level1(), 1920, 1080);
+		}
+	}
+	
+	class ALLevel2 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			Rahmen.inFrame("Level2",new Level1() , 1920, 1080);
+		}
+	}
+	
+	class ALLevel3 implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			Rahmen.inFrame("Level3",new Level1() , 1920, 1080);
+		}
+	}
+}
